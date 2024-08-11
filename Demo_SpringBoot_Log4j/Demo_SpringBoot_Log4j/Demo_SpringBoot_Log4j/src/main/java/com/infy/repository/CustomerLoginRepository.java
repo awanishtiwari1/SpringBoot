34c375dcabeccc2c4aa524004ce3b8b174c57c11
Awanish Tiwari <63773588+awanishtiwari1@users.noreply.github.com>
2024-08-12 00:31:43 +0530

    Delete Demo_SpringBoot_Log4j/Demo_SpringBoot_Log4j/Demo_SpringBoot_Log4j directory

--- Demo_SpringBoot_Log4j/Demo_SpringBoot_Log4j/Demo_SpringBoot_Log4j/src/main/java/com/infy/repository/CustomerLoginRepository.java
+++
@@ -1,7 +0,0 @@
-package com.infy.repository;
-
-import com.infy.dto.CustomerLoginDTO;
-
-public interface CustomerLoginRepository {
-	public CustomerLoginDTO getCustomerLoginByLoginName(String loginName);
-}
