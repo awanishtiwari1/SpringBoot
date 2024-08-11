df740e1e3f1a65fbb1424e89f34a2720be4660bd
Awanish Tiwari <63773588+awanishtiwari1@users.noreply.github.com>
2024-08-12 00:32:03 +0530

    Delete SpringBootLoggingDemo/DemoSpringBootLogging1688455567092/Demo_SpringBoot_Logging directory

--- SpringBootLoggingDemo/DemoSpringBootLogging1688455567092/Demo_SpringBoot_Logging/src/main/java/com/infy/service/CustomerLoginService.java
+++
@@ -1,9 +0,0 @@
-package com.infy.service;
-
-import com.infy.dto.CustomerLoginDTO;
-import com.infy.exception.InfyBankException;
-
-public interface CustomerLoginService {
-	public String authenticateCustomer(CustomerLoginDTO customerLogin)
-			throws InfyBankException;
-}
